41991cb58346468727ac53dd8db0e56a20f99e5b
Casey Klimkowsky <cklimkowsky@gmail.com>
2016-10-12 21:45:32 -0700

    Implemented modal window, and started on photo navigator

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -13,12 +13,44 @@ const photosetId = '72157645388455379';
 
 const flickrUrl = `https://api.flickr.com/services/rest/?method=${apiMethod}&api_key=${apiKey}&photoset_id=${photosetId}&format=json`;
 
+let photosList;
+let currentPhotoIndex;
+
+function showModal(e) {
+  const modal = document.querySelector('.modal');
+
+  let img = document.createElement('img');
+  img.src = e.target.src;
+  img.alt = e.target.alt;
+  img.title = e.target.title;
+
+  document.querySelector('.modal__content').appendChild(img);
+  modal.className += ' modal--visible';
+}
+
+function hideModal(e) {
+  const modal = document.querySelector('.modal');
+  const modalContent = document.querySelector('.modal__content');
+  const modalPhoto = modalContent.lastElementChild;
+
+  modalContent.removeChild(modalPhoto);
+  modal.className = 'modal';
+}
+
+function showPreviousPhoto(e) {
+  console.log(photosList);
+}
+
+function showNextPhoto(e) {
+  console.log(photosList);
+}
+
 function handleResponse(e) {
   const target = e.target;
 
   if (target.readyState == 4 && target.status == 200) {
     let jsonResponse;
-    let photosList;
+    // let photosList;
     let responseText = target.responseText;
 
     // TODO: Use a regex instead
@@ -48,15 +80,30 @@ function handleResponse(e) {
       img.alt = photo.title;
       img.title = photo.title;
       img.className = 'thumbnails-list__thumbnail';
-      img.onclick = function(e) {
-        console.log(e.target.src);
-      };
+      img.onclick = showModal;
 
       listItem.appendChild(img);
       document.querySelector('.thumbnails-list').appendChild(listItem);
     }
   }
 }
+
+const span = document.querySelector('.modal__close');
+span.onclick = hideModal;
+
+const prevPhotoNav = document.querySelector('.navigator--prev');
+prevPhotoNav.onclick = showPreviousPhoto;
+
+const nextPhotoNav = document.querySelector('.navigator--next');
+nextPhotoNav.onclick = showNextPhoto;
+
+window.onclick = function(e) {
+  const modal = document.querySelector('.modal');
+
+  if (e.target === modal) {
+    hideModal(e);
+  }
+};
 
 let xhr = new XMLHttpRequest();
 xhr.onreadystatechange = handleResponse;
